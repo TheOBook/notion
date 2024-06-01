ffe4eb2241d8534b803e76dc57fcd6780fcaeaea
TheOBook <o.benkreira.h@gmail.com>
2024-06-01 13:28:00 -0400

    test1

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// modify this file to avoid the error due to clerk organisation settings
+
 const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
 
 export default clerkMiddleware((auth, request) => {
